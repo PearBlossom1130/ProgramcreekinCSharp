ab8da5b25f228a3203218c82bd6c0efd23770b03
Hwasoo Lee <hlee@hvhc.com>
2017-01-18 16:02:25 -0500

    [MASTER] Working on Isomorphic string

---
+++ ProgramCreek/IsomorphicStrings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace IsomorphicStrings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public static bool IsIsomorphic(string s1, string s2)
+		{
+			if (s1 == null && s2 == null) return true;
+			if (s1 != null && s2 == null) return false;
+			if (s1 == null && s2 != null) return false;
+			if (s1.Length != s2.Length) return false;
+
+			var dic = new Dictionary<char, char>();
+
+			int i = 0;
+			foreach (var c in s1)
+			{
+				if (dic.Keys(c))
+			}
+
+
+
+			return false;
+		}
+	}
+}
